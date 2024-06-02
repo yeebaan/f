cb506cc8ebd4adc1880016ee2cd74c21c4816004
yeebaan <165865970+yeebaan@users.noreply.github.com>
2024-06-02 23:15:31 +0800

    Add files via upload

--- hls/SobelFilter.cpp
+++ hls/SobelFilter.cpp
@@ -22,21 +22,22 @@ void SobelFilter::do_filter()
     o_result.reset();
     wait();
   }
-  constexpr auto depth{8};
-  constexpr auto width{16};
-  std::array<std::array<sc_uint<4>, width>, depth> t{};
+  constexpr auto depth{4};
+  constexpr auto width{depth * 2};
+  constexpr auto n{4};
+  std::array<std::array<sc_uint<n>, width>, depth> t{};
   #ifndef NATIVE_SYSTEMC
     HLS_FLATTEN_ARRAY(t);
   #endif
   for (auto i{0u}; i < t.size(); i++) {
     #ifndef NATIVE_SYSTEMC
-      HLS_UNROLL_LOOP(ALL,"loop1");
+      HLS_UNROLL_LOOP(ALL, "loop1");
       auto tmp{i_rgb.get()};
     #else
       auto tmp{i_rgb.read()};
     #endif
     for (auto j{0u}; j < t[0].size(); j++) {
-      t[i][j] = tmp.range(3 + 4 * j, 4 * j);
+      t[i][j] = tmp.range(n * (j + 1) - 1, n * j);
     }
   }
   std::array<std::array<std::array<std::array<bool, 2>, 2>, width>, depth> u{};
@@ -45,7 +46,7 @@ void SobelFilter::do_filter()
   #endif
   for (auto i{0u}; i < u.size(); i++) {
     #ifndef NATIVE_SYSTEMC
-      HLS_UNROLL_LOOP(ALL,"loop2");
+      HLS_UNROLL_LOOP(ALL, "loop2");
       auto tmp{i_rgb.get()};
     #else
       auto tmp{i_rgb.read()};
@@ -53,7 +54,7 @@ void SobelFilter::do_filter()
     for (auto j{0u}; j < u[0].size(); j++) {
       for (auto k{0u}; k < u[0][0].size(); k++) {
         for (auto l{0u}; l < u[0][0][0].size(); l++) {
-          u[i][j][k][l] = tmp[2 * (2 * j + k) + l];
+          u[i][j][k][l] = tmp[u[0][0][0].size() * (u[0][0].size() * j + k) + l];
         }
       }
     }
@@ -61,13 +62,8 @@ void SobelFilter::do_filter()
   while (true)
   {
     #ifndef NATIVE_SYSTEMC
-      //HLS_PIPELINE_LOOP(HARD_STALL, 1, "pipe0");
+      HLS_PIPELINE_LOOP(HARD_STALL, 1, "pipe0");
       HLS_CONSTRAIN_LATENCY(0, HLS_ACHIEVABLE, "lat0");
-    #endif
-    #ifndef NATIVE_SYSTEMC
-      auto tmp{i_rgb.get()};
-    #else
-      auto tmp{i_rgb.read()};
     #endif
     std::array<std::array<bool, width>, depth> a{};
     #ifndef NATIVE_SYSTEMC
@@ -77,12 +73,25 @@ void SobelFilter::do_filter()
     #ifndef NATIVE_SYSTEMC
       HLS_FLATTEN_ARRAY(b);
     #endif
+    #ifndef NATIVE_SYSTEMC
+      auto tmp{i_rgb.get()};
+    #else
+      auto tmp{i_rgb.read()};
+    #endif
+    std::array<bool, width> c{};
+    #ifndef NATIVE_SYSTEMC
+      HLS_FLATTEN_ARRAY(c);
+    #endif
+    for (auto i{0u}; i < c.size(); i++)
+    {
+      c[i] = tmp[i];
+    }
     for (auto i{0u}; i < b.size(); i++)
     {
       for (auto j{0u}; j < b[0].size(); j++)
       {
-        a[i][j] = (i == 0) ? tmp[t[i][j]] : b[i - 1][t[i][j]];
-        b[i][j] = (j == 0) ? u[i][j][a[i][j]][0] : u[i][j][a[i][j]][a[i][j - 1]];
+        a[i][j] = ((i == 0) ? c : (b[i - 1]))[t[i][j]];
+        b[i][j] = u[i][j][a[i][j]][a[i][(j == 0) ? 0 : (j - 1)]];
       }
     }
     result_t output{};
--- riscv-vp/vp/src/platform/basic-acc/filter_def.h
+++ riscv-vp/vp/src/platform/basic-acc/filter_def.h
@@ -16,7 +16,7 @@ const int DMA_TRANS = 64;
 // Sobel Filter inner transport addresses
 // Used between blocking_transport() & do_filter()
 const int SOBEL_FILTER_R_ADDR = 0x00000000;
-const int SOBEL_FILTER_RESULT_ADDR = 0x00000010;
+const int SOBEL_FILTER_RESULT_ADDR = 0x00000100;
 
 const int SOBEL_FILTER_RS_R_ADDR   = 0x00000000;
 const int SOBEL_FILTER_RS_W_WIDTH  = 0x00000004;
