7d17527c4d3d92d220ce5fe69aa16a3a774ae9d6
yeebaan <165865970+yeebaan@users.noreply.github.com>
2024-06-10 18:01:37 +0800

    Add files via upload

--- riscv-vp/sw/basic-multicore/main.c
+++ riscv-vp/sw/basic-multicore/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
-int sem_init (uint32_t *__sem, uint32_t count) __THROW
+int sem_init(uint32_t *__sem, uint32_t count) __THROW
 {
-  *__sem=count;
+  *__sem = count;
   return 0;
 }
-int sem_wait (uint32_t *__sem) __THROW
+int sem_wait(uint32_t *__sem) __THROW
 {
-  uint32_t value, success; //RV32A
+  uint32_t value, success; // RV32A
   __asm__ __volatile__("\
 L%=:\n\t\
      lr.w %[value],(%[__sem])            # load reserved\n\t\
@@ -16,14 +16,14 @@ L%=:\n\t\
      sc.w %[success],%[value],(%[__sem]) # store conditionally\n\t\
      bnez %[success], L%=                # if the store failed, try again\n\t\
 "
-    : [value] "=r"(value), [success]"=r"(success)
-    : [__sem] "r"(__sem)
-    : "memory");
+                       : [value] "=r"(value), [success] "=r"(success)
+                       : [__sem] "r"(__sem)
+                       : "memory");
   return 0;
 }
-int sem_post (uint32_t *__sem) __THROW
+int sem_post(uint32_t *__sem) __THROW
 {
-  uint32_t value, success; //RV32A
+  uint32_t value, success; // RV32A
   __asm__ __volatile__("\
 L%=:\n\t\
      lr.w %[value],(%[__sem])            # load reserved\n\t\
@@ -31,44 +31,23 @@ L%=:\n\t\
      sc.w %[success],%[value],(%[__sem]) # store conditionally\n\t\
      bnez %[success], L%=                # if the store failed, try again\n\t\
 "
-    : [value] "=r"(value), [success]"=r"(success)
-    : [__sem] "r"(__sem)
-    : "memory");
+                       : [value] "=r"(value), [success] "=r"(success)
+                       : [__sem] "r"(__sem)
+                       : "memory");
   return 0;
 }
-int barrier(uint32_t *__sem, uint32_t *__lock, uint32_t *counter, uint32_t thread_count) {
-	sem_wait(__lock);
-	if (*counter == thread_count - 1) { //all finished
-		*counter = 0;
-		sem_post(__lock);
-		for (uint32_t j = 0; j < thread_count - 1; ++j) sem_post(__sem);
-	} else {
-		(*counter)++;
-		sem_post(__lock);
-		sem_wait(__sem);
-	}
-	return 0;
-}
-//Total number of cores
-//static const int PROCESSORS = 2;
-#define PROCESSORS 2
-//the barrier synchronization objects
-uint32_t barrier_counter=0; 
-uint32_t barrier_lock; 
-uint32_t barrier_sem; 
-//the mutex object to control global summation
-uint32_t lock;  
-//print synchronication semaphore (print in core order)
-uint32_t print_sem[PROCESSORS];
-static uint8_t *const SOBEL_FILTER_R_ADDR = (uint8_t *)(0x04000000 + 0x000);
-static uint8_t *const SOBEL_FILTER_RESULT_ADDR = (uint8_t *)(0x04000000 + 0x100);
-static uint8_t *const SOBEL_FILTER_R_ADDR_2 = (uint8_t *)(0x05000000 + 0x000);
-static uint8_t *const SOBEL_FILTER_RESULT_ADDR_2 = (uint8_t *)(0x05000000 + 0x100);
+uint32_t lock;
+static const uint32_t DMA_OP_MEMCPY = 1;
 static volatile uint32_t *const DMA_SRC_ADDR = (uint32_t *)(0x03000000 + 0);
 static volatile uint32_t *const DMA_DST_ADDR = (uint32_t *)(0x03000000 + 4);
 static volatile uint32_t *const DMA_LEN_ADDR = (uint32_t *)(0x03000000 + 8);
 static volatile uint32_t *const DMA_OP_ADDR = (uint32_t *)(0x03000000 + 12);
-static const uint32_t DMA_OP_MEMCPY = 1;
+static uint8_t *const SOBEL_FILTER_R_ADDR = (uint8_t *)(0x04000000 + 0x000);
+static uint8_t *const SOBEL_FILTER_RESULT_ADDR = (uint8_t *)(0x04000000 + 0x100);
+static uint8_t *const SOBEL_FILTER_R_NUM_ADDR = (uint8_t *)(0x04000000 + 0x200);
+static uint8_t *const SOBEL_FILTER_R_ADDR_2 = (uint8_t *)(0x05000000 + 0x000);
+static uint8_t *const SOBEL_FILTER_RESULT_ADDR_2 = (uint8_t *)(0x05000000 + 0x100);
+static uint8_t *const SOBEL_FILTER_R_NUM_ADDR_2 = (uint8_t *)(0x05000000 + 0x200);
 void write_data_to_ACC(uint8_t *ADDR, volatile uint8_t *buffer, int len)
 {
   *DMA_SRC_ADDR = (uint32_t)(buffer);
@@ -88,86 +67,102 @@ void read_data_from_ACC(uint8_t *ADDR, volatile uint8_t *buffer, int len)
 #define dut_to_tb_size (io_width + 1)
 static volatile uint8_t tb_to_dut[tb_to_dut_size] = {0};
 static volatile uint8_t dut_to_tb[dut_to_tb_size] = {0};
-int main(int hart_id, char ** unused)
+static volatile uint8_t tb_to_dut_2[tb_to_dut_size] = {0};
+static volatile uint8_t dut_to_tb_2[dut_to_tb_size] = {0};
+int main(int hart_id, char **unused)
 {
-  if (hart_id == 0) {
-    volatile uint64_t a = 1;
-    a++;
+  if (hart_id == 0)
+  {
+    sem_init(&lock, 1);
   }
-  if (hart_id == 0) {
-		// create a barrier object with a count of PROCESSORS
-		sem_init(&barrier_lock, 1);
-		sem_init(&barrier_sem, 0); //lock all cores initially
-		for(int i=0; i< PROCESSORS; ++i){
-			sem_init(&print_sem[i], 0); //lock printing initially
-		}
-		// Create mutex lock
-		sem_init(&lock, 1);
-	}
-  uint8_t * r_addr = (hart_id == 0) ? SOBEL_FILTER_R_ADDR : SOBEL_FILTER_R_ADDR_2;
-  uint8_t * result_addr = (hart_id == 0) ? SOBEL_FILTER_RESULT_ADDR : SOBEL_FILTER_RESULT_ADDR_2;
+  uint8_t *r_addr = (hart_id == 0) ? SOBEL_FILTER_R_ADDR : SOBEL_FILTER_R_ADDR_2;
+  uint8_t *result_addr = (hart_id == 0) ? SOBEL_FILTER_RESULT_ADDR : SOBEL_FILTER_RESULT_ADDR_2;
+  uint8_t *r_num_addr = (hart_id == 0) ? SOBEL_FILTER_R_NUM_ADDR : SOBEL_FILTER_R_NUM_ADDR_2;
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 010210 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 010210 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 042121 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 042121 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 0431 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 0431 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 0210 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 0210 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 0b10000000110001100000 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 0b10000000110001100000 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 0b11001000000001100000 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 0b11001000000001100000 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 0b111010100000 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 0b111010100000 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
   for (int i = 0; i < tb_to_dut_size; i++)
   {
-    tb_to_dut[i] = 0b110011000000 >> (8 * i);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[i] = 0b110011000000 >> (8 * i);
   }
-  write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
+  write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
+  sem_wait(&lock);
   (void)((hart_id == 0) && (printf("                             \n")));
   (void)((hart_id == 0) && (printf("        |          |         \n")));
   (void)((hart_id == 0) && (printf("hart_id |  A B Cin |  S Cout \n")));
-  for (int i = (hart_id == 0) ? 0 : 4; i < ((hart_id == 0) ? 4 : 8); i++)
+  sem_post(&lock);
+  for (int i = (hart_id == 0) ? 1 : 0; i < 8; i += 2)
   {
-    tb_to_dut[0] = i;
-    write_data_to_ACC(r_addr, tb_to_dut, tb_to_dut_size);
-    dut_to_tb[0] = 0;
-    read_data_from_ACC(result_addr, dut_to_tb, dut_to_tb_size);
+    ((hart_id == 0) ? tb_to_dut : tb_to_dut_2)[0] = i;
+    write_data_to_ACC(r_addr, ((hart_id == 0) ? tb_to_dut : tb_to_dut_2), tb_to_dut_size);
+    while (1)
+    {
+      ((hart_id == 0) ? dut_to_tb : dut_to_tb_2)[0] = 0;
+      read_data_from_ACC(r_num_addr, ((hart_id == 0) ? dut_to_tb : dut_to_tb_2), dut_to_tb_size);
+      uint8_t sample[dut_to_tb_size] = {0};
+      while (1)
+      {
+        for (int i = 0; i < dut_to_tb_size; i++)
+        {
+          sample[i] = ((hart_id == 0) ? dut_to_tb : dut_to_tb_2)[i];
+        }
+        if (1 == sample[0])
+        {
+          break;
+        }
+      }
+      if (0 < sample[1])
+      {
+        break;
+      }
+    }
+    ((hart_id == 0) ? dut_to_tb : dut_to_tb_2)[0] = 0;
+    read_data_from_ACC(result_addr, ((hart_id == 0) ? dut_to_tb : dut_to_tb_2), dut_to_tb_size);
     uint8_t sample[dut_to_tb_size] = {0};
     while (1)
     {
       for (int i = 0; i < dut_to_tb_size; i++)
       {
-        sample[i] = dut_to_tb[i];
+        sample[i] = ((hart_id == 0) ? dut_to_tb : dut_to_tb_2)[i];
       }
       if (1 == sample[0])
       {
         break;
       }
     }
-    barrier(&barrier_sem, &barrier_lock, &barrier_counter, PROCESSORS);
+    sem_wait(&lock);
     printf("    ");
     printf("%d", hart_id);
     printf("   |  ");
@@ -181,7 +176,9 @@ int main(int hart_id, char ** unused)
     printf(" ");
     printf("%d", (sample[1] >> 2) & 1);
     printf("\n");
+    sem_post(&lock);
   }
-  barrier(&barrier_sem, &barrier_lock, &barrier_counter, PROCESSORS);
+  sem_wait(&lock);
   (void)((hart_id == 0) && (printf("        |          |         \n")));
+  sem_post(&lock);
 }
